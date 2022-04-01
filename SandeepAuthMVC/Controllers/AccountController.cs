dd9ede5b843dadc9c432d132e7be7c2baf1af3e4
SANDEEP54654 <sandeep54654@gmail.com>
2022-04-01 12:04:55 -0400

    Add project files.

---
+++ SandeepAuthMVC/Controllers/AccountController.cs
@@ -0,0 +1,125 @@
+﻿using SandeepAuthMVC.Models;
+using SandeepAuthMVC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SandeepAuthMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SaveRegisterDetails(Register registerDetails)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //create database context using Entity framework 
+                using (var databaseContext = new SandeepAuthMVCEntities())
+                {
+                    RegisterUser reglog = new RegisterUser();
+
+
+                    reglog.FirstName = registerDetails.FirstName;
+                    reglog.LastName = registerDetails.LastName;
+                    reglog.Email = registerDetails.Email;
+                    reglog.Password = registerDetails.Password;
+
+
+
+                    databaseContext.RegisterUsers.Add(reglog);
+                    databaseContext.SaveChanges();
+                }
+
+                ViewBag.Message = "User Details Saved";
+                return View("Register");
+            }
+            else
+            {
+
+                return View("Register", registerDetails);
+            }
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var isValidUser = IsValidUser(model);
+
+
+                if (isValidUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Email, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    //If the username and password combination is not present in DB then error message is shown.
+                    ModelState.AddModelError("Failure", "Wrong Username and password combination !");
+                    return View();
+                }
+            }
+            else
+            {
+                //If model state is not valid, the model with error message is returned to the View.
+                return View(model);
+            }
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+
+        public RegisterUser IsValidUser(LoginViewModel model)
+        {
+            using (var dataContext = new SandeepAuthMVCEntities())
+            {
+                //Retireving the user details from DB based on username and password enetered by user.
+                RegisterUser user = dataContext.RegisterUsers.Where(query => query.Email.Equals(model.Email) && query.Password.Equals(model.Password)).SingleOrDefault();
+                //If user is present, then true is returned.
+                if (user == null)
+                    return null;
+                //If user is not present false is returned.
+                else
+                    return user;
+            }
+        }
+
+        
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon(); // it will clear the session at the end of request
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SandeepAuthMVC/ViewModel/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SandeepAuthMVC.ViewModel
+{
+    public class TestController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("Failure", "Please Login !");
+                return View();
+            }
+        }
+        
+    }
+}
